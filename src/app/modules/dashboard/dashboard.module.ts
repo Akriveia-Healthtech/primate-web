4a6cdb03ab9afd57ec94565b297238611e4cec61
Prajeet-Shrestha <prazzeettstha@gmail.com>
2021-06-04 10:01:17 +0545

    menu

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -6,9 +6,10 @@ import { PostComponent } from './post/post.component';
 import { SettingComponent } from './setting/setting.component';
 import { StatsComponent } from './stats/stats.component';
 import { CommonComponentsModule } from 'src/app/core/commonComponents/commomComponents.module';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 
 @NgModule({
   declarations: [PostComponent, SettingComponent, StatsComponent],
-  imports: [CommonModule, AppRoutingModule, CommonComponentsModule],
+  imports: [CommonModule, MatDatepickerModule, AppRoutingModule, CommonComponentsModule],
 })
 export class DashboardModule {}
