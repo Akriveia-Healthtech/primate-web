4bd13fe44ca69fd0732ae5ecaabf803363801018
Prajeet-Shrestha <prazzeettstha@gmail.com>
2021-06-04 18:23:11 +0545

    Adding header to the request

--- src/app/core/services/http/http.service.ts
+++ src/app/core/services/http/http.service.ts
@@ -3,6 +3,7 @@ import { catchError } from 'rxjs/operators';
 import { observable, throwError } from 'rxjs';
 import { HttpClient, HttpClientModule, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { UtilityService } from '../../utility/utility.service';
 @Injectable({
   providedIn: 'root',
 })
@@ -10,7 +11,21 @@ export class HttpService {
   constructor(private _http: HttpClient, private router: Router) {}
 
   post(url: string, obj: any) {
-    return this._http.post<any>(url, obj).pipe(catchError(this.handleError));
+    let headers;
+    let options = {};
+    try {
+      const token = JSON.parse(localStorage.getItem('TId'));
+      console.log(token);
+      if (token === null) {
+      } else {
+        headers = new HttpHeaders({
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + token,
+        });
+        options = { headers: headers };
+      }
+    } catch (e) {}
+    return this._http.post<any>(url, obj, options).pipe(catchError(this.handleError));
   }
   get(url: string) {
     return this._http.get(url).pipe(catchError(this.handleError));
