1b2cc856f02d255d618beb336dd748164a5a477c
Prajeet-Shrestha <prazzeettstha@gmail.com>
2021-06-04 23:29:17 +0545

    dashboard clean up

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -7,9 +7,20 @@ import { SettingComponent } from './setting/setting.component';
 import { StatsComponent } from './stats/stats.component';
 import { CommonComponentsModule } from 'src/app/core/commonComponents/commomComponents.module';
 import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
   declarations: [PostComponent, SettingComponent, StatsComponent],
-  imports: [CommonModule, MatDatepickerModule, AppRoutingModule, CommonComponentsModule],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    AppRoutingModule,
+    CommonComponentsModule,
+    MatNativeDateModule,
+  ],
 })
 export class DashboardModule {}
