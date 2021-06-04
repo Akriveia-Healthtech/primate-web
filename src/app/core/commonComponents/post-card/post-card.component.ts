2ca2d7b46cf003197fb597a7b9f8d9970d659b7f
Prajeet-Shrestha <prazzeettstha@gmail.com>
2021-06-04 23:25:08 +0545

    Dynamix addition to card as a template

--- src/app/core/commonComponents/post-card/post-card.component.ts
+++ src/app/core/commonComponents/post-card/post-card.component.ts
@@ -6,8 +6,21 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./post-card.component.css'],
 })
 export class PostCardComponent implements OnInit {
-  constructor() {}
+  constructor() {
+    // console.log(new Date(this.createdDate * 1000), this.createdDate);
+    // this.createdDate
+  }
 
   ngOnInit(): void {}
   @Input() draft: boolean = false;
+  @Input() postID: string;
+  @Input() slug: string;
+  @Input() createdDate: number;
+  @Input() reads: number;
+  @Input() votes: number;
+  @Input() title: string;
+  @Input() description: string;
+  @Input() featuredImg: string | null;
+  @Input() status: string;
+  @Input() pinned: boolean = false;
 }
